acb1e950d547c0c18f65ebf88e3e27ad117ab8f0
hmatsu47 <hmatsu47@gmail.com>
2022-08-14 15:32:53 +0900

    API 呼び出しのテストコードを再調整

--- src/fetchData.test.ts
+++ src/fetchData.test.ts
@@ -2,7 +2,7 @@
 
 import { beforeEach, describe, expect, test } from "vitest";
 import "vi-fetch/setup";
-import { mockFetch } from "vi-fetch";
+import { mockPost } from "vi-fetch";
 import { fetchData } from "./fetchData";
 import {
   abura,
@@ -15,21 +15,17 @@ import {
 import { OrderRequest, OrderResponse } from "./type";
 
 describe("fetchData", () => {
-  beforeEach(() => {
-    mockFetch.clearAll();
-  });
   test("API 呼び出し", async () => {
-    const mock = mockFetch(
-      "POST",
-      "http://localhost:8080/JiroApi/AcceptOrder"
-    ).willResolve({
-      noodle: 300,
-      chaSiuPork: 2,
-      vegetable: 1.0,
-      garlic: 1.0,
-      fat: 1.0,
-      kaeshi: 1.0,
-    } as OrderResponse);
+    const mock = mockPost("http://localhost:8080/JiroApi/AcceptOrder")
+      .withHeaders([["Content-Type", "application/json;charset=utf8"]])
+      .willResolve({
+        noodle: 300,
+        chaSiuPork: 2,
+        vegetable: 1.0,
+        garlic: 1.0,
+        fat: 1.0,
+        kaeshi: 1.0,
+      } as OrderResponse);
     await fetchData();
     // とりあえず呼び出しが行われたことだけを確認
     expect(mock).toHaveFetched();
@@ -41,5 +37,6 @@ describe("fetchData", () => {
       abura: abura(),
       karame: karame(),
     } as OrderRequest);
+    mock.clear();
   });
 });
